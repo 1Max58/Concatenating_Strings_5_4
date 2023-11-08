5197c89d8b7084e54383d7e088c977eafaaa6ba4
1Max58 <>
2023-11-08 18:37:46 +0200

    Input and concatenation of strings implemented

--- src/com/example/concatenatingstrings/ConcatenatingStrings.java
+++ src/com/example/concatenatingstrings/ConcatenatingStrings.java
@@ -1,7 +1,20 @@
 package com.example.concatenatingstrings;
 
+import java.util.Scanner;
+
 public class ConcatenatingStrings {
     public static void main(String[] args) {
-
+        System.out.println("This program concatenates two strings.");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter two strings.");
+        System.out.print("First string: ");
+        String first = input.next();
+        System.out.print("Second string: ");
+        String second = input.next();
+        String firstAddedToSecond = second.concat(first);
+        String secondAddedToFirst = first.concat(second);
+        System.out.print("The first one added to the second: " + firstAddedToSecond);
+        System.out.println();
+        System.out.print("The second one added to the first: " + secondAddedToFirst);
     }
 }
